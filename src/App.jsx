5c19892368445098952b8d58f7017d2c98c7ebd4
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-07-19 17:15:06 -0300

    Se agregan estilos, iconos, api en un json, se renderizan productos y filtrado de los mismos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import './App.css'
+import { products as initialProducts } from './mocks/products.json'
+import { Products } from "./components/Products"
+import { useState } from 'react'
 
 function App() {
+    const [products] = useState(initialProducts)
+    const [filters, setFilters] = useState({
+        category: 'all',
+        minPrice: 0
+    })
 
-  return (
-    <>
+    const filterProducts = (products) => {
+        return products.filter(product => {
+            return (
+                product.price >= filters.minPrice && 
+                (
+                    filters.category === 'all' ||
+                    product.category === filters.category
+                )
+            )
+        })
+    }
 
-    </>
-  )
+    const filteredProducts = filterProducts(products)
+
+    return (
+        <Products products={filteredProducts} />
+    )
 }
 
 export default App
