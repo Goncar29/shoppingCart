7b289ad3b9fb4a7049563aaad2dbcc5838ae7c9a
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-07-20 15:28:32 -0300

    se completo el filtro por categoria y precio

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,21 +1,41 @@
+import { useState } from 'react'
 import './Filters.css'
 
-export function Filters () {
+export function Filters ({ onChange }) {
+    const [minPrice, setMinPrice] = useState(0)
+    
+    const handleChangeMinPrice = (event) => {
+        setMinPrice(event.target.value)
+        onChange(prevState => ({
+            ...prevState,
+            minPrice: event.target.value
+        }))
+    }
+
+    const handleChangeCategory = (event) => {
+        onChange(prevState => ({
+            ...prevState,
+            category: event.target.value
+        }))
+    }
+
     return (
         <section className="filters">
             <div>
-                <label htmlFor="price">Price</label>
+                <label htmlFor="price">Precio desde: </label>
                 <input 
                     type="range"
                     id="price"
                     min="0"
                     max="1000"
+                    onChange={handleChangeMinPrice}
                 />
+                <span>${minPrice}</span>
             </div>
 
             <div>
                 <label htmlFor="category">Categoria</label>
-                <select id="category">
+                <select id="category" onChange={handleChangeCategory}>
                     <option value="all">Todas</option>
                     <option value="laptops">Computadoras</option>
                     <option value="smartphones">Celulares</option>
