611f01910ce44d71a86dcfff28e17c035df22062
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-07-20 12:48:41 -0300

    separacion de componentes, estilos y creación del header

---
+++ src/components/Filters.jsx
@@ -0,0 +1,26 @@
+import './Filters.css'
+
+export function Filters () {
+    return (
+        <section className="filters">
+            <div>
+                <label htmlFor="price">Price</label>
+                <input 
+                    type="range"
+                    id="price"
+                    min="0"
+                    max="1000"
+                />
+            </div>
+
+            <div>
+                <label htmlFor="category">Categoria</label>
+                <select id="category">
+                    <option value="all">Todas</option>
+                    <option value="laptops">Computadoras</option>
+                    <option value="smartphones">Celulares</option>
+                </select>
+            </div>
+        </section>
+    )
+}
